b157ad724f73d9baf621ba5d4c1ffc3a27c24948
Michael Roose <mjroose@gmail.com>
2018-08-13 02:17:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,4 +7,15 @@ class Song
     @@all << self
   end
 
+  def self.new_by_filename(filename)
+    filename.slice!("mp3")
+    song_data = filename.split(" - ")
+    artist = song[0]
+    name = song[1]
+    genre = song[2]
+    song = self.new(name)
+    song.artist = artist
+    song.genre = genre
+    song
+  end
 end
